41e636d9b3c719d033810cf2a3c33bd5e193388f
Mireille0108 <EmbohMireill07@gmail.com>
2022-07-11 22:24:40 -0700

    myexam

---
+++ game.js
@@ -0,0 +1,43 @@
+let listOfQuestion = [
+    {
+        q:'the question 1',
+        a:'the answer 1'
+    }
+
+    {
+      q:'the question n',
+      a:'the answer n',
+    }
+]
+let input, output, score, question//declearing variables
+saveaGame = [];
+
+function initUI () {
+    input = document.getElementById("scores");
+    output = document.getElementById("ouput");
+    score = document .getElementById(score);
+    question = document.getElementById(question);
+}
+
+function giveValueToGame(listingQuestion) {
+    const saveaGame = {
+        input:input.value,
+        output: output.value,
+        score:score.vale,
+        question:question.value,
+        id:new Date() . valueOf()
+       
+        
+
+    }
+return output
+}
+saveaGame.push()
+
+for (let i = 50%; i < saveaGame.length; i++) {
+    const element = array[index];
+    
+}
+
+
+
